59d16d30dc15cef294c8a4b8d83dd6704a161d94
Alexander Melnikov <alexander_melnikov@epam.com>
2024-09-16 20:17:40 -0400

    Add Auth0

--- Budget/Budget.Web.UI/Program.cs
+++ Budget/Budget.Web.UI/Program.cs
@@ -1,4 +1,7 @@
+using Auth0.AspNetCore.Authentication;
 using Budget.Web.UI.Components;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Budget.Web.UI;
 
@@ -8,11 +11,23 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddAuth0WebAppAuthentication(options =>
+        {
+            options.Domain = builder.Configuration["Auth0:Domain"];
+            options.ClientId = builder.Configuration["Auth0:ClientId"];
+        });
+
         // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
+        
+        builder.Services.AddCascadingAuthenticationState();
 
         var app = builder.Build();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
@@ -29,6 +44,21 @@ public class Program
 
         app.MapRazorComponents<App>()
             .AddInteractiveServerRenderMode();
+
+        app.MapGet("/Account/Login", async (HttpContext httpContext, string returnUrl = "/") =>
+        {
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder().WithRedirectUri(returnUrl).Build();
+
+            await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        });
+
+        app.MapGet("/Account/Logout", async httpContext =>
+        {
+            var authenticationProperties = new LogoutAuthenticationPropertiesBuilder().WithRedirectUri("/").Build();
+
+            await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+            await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        });
 
         app.Run();
     }
